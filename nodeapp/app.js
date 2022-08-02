1acd1a1d992bb9c7d99363987dba5d6f97ccc5c8
Bassem-Kamel <96839515+Bassem-Kamel@users.noreply.github.com>
2022-08-02 20:11:32 +0200

    Update app.js

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
-    res.send('<h1> ITI-graduation-project hosted by Bassemkamel</h1>')
+    res.send('Hello World from ITI 3 month')
   })
   
   app.listen(port, () => {
